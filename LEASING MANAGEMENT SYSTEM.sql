79f97c7fdff6d9570ae4e88b58859a5385ea5f9a
Sibel Yıldız <sibel_yldz@yahoo.com>
2024-11-26 17:57:38 +0300

    Add files via upload

---
+++ LEASING MANAGEMENT SYSTEM.sql
@@ -0,0 +1,12 @@
+select *from Customers
+select *from Vehicles
+where Vehicletrademark ='Audi'
+select Customers.Customername,Customers.Customersurname,Vehicles.Vehicletrademark
+from Leasing join Customers on Leasing.CustomerID=Customers.CustomerID
+join Vehicles on Leasing.VehicleID=Vehicles.VehicleID
+where Customers.Customername='Kenan'
+select Vehicletrademark,MAX(Price) as max_price,Vehiclemodel from Vehicles
+group by Vehicletrademark,Vehiclemodel
+order by max_price desc
+select Customers.Customername,Customers.Customersurname,DATEDIFF(MONTH,Leasing.Finaldate,Leasing.Startdate)
+from Leasing join Customers on Leasing.CustomerID=Customers.CustomerID
