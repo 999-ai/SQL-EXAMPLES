cdbf2a9ba1995f6b27e60595abf70212f8c669e3
Sibel Yıldız <sibel_yldz@yahoo.com>
2024-11-30 12:21:11 +0300

    Add files via upload

---
+++ Restaurant Reservation System.sql
@@ -0,0 +1,24 @@
+select Customers.Customername 
+from Customers join Reservations on Customers.CustomerID=Reservations.CustomerID
+select Reservations.ReservationID from Reservations
+select ReservationID,count(Reservations.ReservationID) as reservation_number from Reservations
+group by ReservationID
+order by reservation_number asc
+select Customers.Customername,Reservations.Reservartiondate 
+from Customers join Reservations on Customers.CustomerID=Reservations.CustomerID
+where Customers.Customername='Jason' 
+and Reservations.Reservartiondate  < '2020-01-01'
+group by Customers.Customername,Reservations.Reservartiondate 
+select max(reservation_count) as maxreservation_number
+from (select Reservations.ReservationID, count(Reservations.ReservationID) as reservation_count
+    from Reservations
+    group by Reservations.ReservationID)
+	as subquery 
+
+
+
+
+
+
+
+ 
